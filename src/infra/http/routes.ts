e0385cf8fea51d59e3075f0c03d00de1c980b4d2
jonasexplore <jonasexplore@gmail.com>
2021-08-21 18:44:01 -0300

    feat: add new routes

--- src/infra/http/routes.ts
+++ src/infra/http/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
-import { createShortenedLinkController } from "../../domain/useCases/CreateShortenedLink";
-import { createShortenedQRCodeController } from "../../domain/useCases/CreateShortenedQRCode";
-import { showShortenedLinkController } from "../../domain/useCases/ShowShortenedLink";
+import { createShortenedLinkController } from "../../domain/links/useCases/CreateShortenedLink";
+import { createShortenedQRCodeController } from "../../domain/links/useCases/CreateShortenedQRCode";
+import { showShortenedLinkController } from "../../domain/links/useCases/ShowShortenedLink";
+import { createUserController } from "../../domain/users/useCases/CreateUser";
+import { getUserController } from "../../domain/users/useCases/GetUser";
 
 const routes = Router();
 
 routes.post("/short", createShortenedLinkController.handle);
 routes.get("/short/:id", showShortenedLinkController.handle);
 routes.get("/qrcode/:id", createShortenedQRCodeController.handle);
+
+routes.post('/users', createUserController.handle);
+routes.get('/users', getUserController.handle);
 
 export { routes };
